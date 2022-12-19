d7fa176552bb148e62dabe8e6d7e4d64618cf077
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-19 12:02:31 +0100

    Generati 16 numeri random unici

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
-/*
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-#MILESTONE 1
-Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
-#MILESTONE 2
-Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
-#MILESTONE 3
-In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
-#MILESTONE 4
-Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
-*/
-
 //Creo funzione per generare le celle
-const createCell = (howMany, content) =>
-  (cell = `<div class="cell cell-${howMany}">${content}</div>`);
+const createCell = (size, content) =>
+  (cell = `<div class="cell cell-${size}">${content}</div>`);
 
 //Creo funzione per il click
 const getCliccable = (element) => {
@@ -23,6 +8,17 @@ const getCliccable = (element) => {
     element.classList.toggle("clicked");
     console.log(element.textContent);
   });
+};
+
+//Creo funzione per generare numeri random unici in un range
+
+const getRandomNumbersNotDuplicates = (min = 1, max = 100, numberList) => {
+  max++;
+  let randomNumber;
+  do {
+    randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  } while (numberList.includes(randomNumber));
+  return randomNumber;
 };
 
 //Targhettizzo gli elementi in pagina
@@ -52,6 +48,17 @@ button.addEventListener("click", function () {
     cells += createCell(level, i);
   }
   table.innerHTML = cells;
+
+  //Creo array per lista bombe
+  let bombs = [];
+
+  //Genero numeri da inserire nelle bombe
+  for (let i = 0; i < 16; i++) {
+    const randomNumber = getRandomNumbersNotDuplicates(1, cellsNumber, bombs);
+    bombs.push(randomNumber);
+  }
+
+  console.log(bombs);
 
   const cellsElement = document.querySelectorAll("#table .cell");
   //Modifico colonne al click
